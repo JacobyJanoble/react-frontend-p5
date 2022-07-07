6a1786d1ebd367809d011a2892f6899af2a50887
Jacob <Jacob.McCann214@gmail.com>
2022-07-07 10:52:13 -0500

    Created fetch requests for users, channels, and posts. Imported components to prep for routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { BrowserRouter as Router, Route, Redirect, Routes } from 'react-router-dom';
+
+import Signup from './components/SignUp';
+import Login from './components/Login';
+import Dashboard from './containers/Dashboard';
+import NewPostPage from './components/NewPostPage';
+import UserPage from './components/UserPage';
+import PostPage from './components/PostPage';
+import NewChannelPage from './components/NewChannelPage';
+import ChannelList from './containers/ChannelList';
+import ChannelPage from './components/ChannelPage';
 
 function App() {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    let users
+    let channels
+
+    fetch('http://localhost:3000/users')
+    .then(resp => resp.json())
+    .then(userData => {
+      users = userData
+
+      fetch('http://localhost:3000/channels')
+      .then(resp2 => resp2.json())
+      .then(channelData => {
+        channels = channelData
+
+        fetch('http://localhost:3000/posts')
+        .then(resp3 => resp3.json())
+        .then(data => {
+          dispatch({type: 'GET_POSTS', posts: data})
+          dispatch({type: 'GET_CHANNELS', channels: channels})
+          dispatch({type: 'GET_USERS', users: users})
+          dispatch({type: 'LOGIN', username:localStorage.getItem('currentUserUsername')})
+          console.log("Finish fetching")
+        })
+      })
+    })
+  }, [])
+
+  const users = useSelector(state => state.user.allUsers)
+  const useSelector(state => state.user.allUsers)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+
+
+
+      </Routes>
     </div>
   );
 }
