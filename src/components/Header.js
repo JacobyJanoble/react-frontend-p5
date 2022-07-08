719fc935d7819e1441a3b027993a9e5c076cc857
Jacob <Jacob.McCann214@gmail.com>
2022-07-08 11:02:33 -0500

    Added code to Header, still need to work on links and handle search function

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from 'react';
+import { Link, link } from 'react-router-dom';
 
-const Header = () => {
+import { useSelector } from 'react-redux';
+
+const Header = (props) => {
+    const currentUser = useSelector(state => state.user.currentUser)
+
+    const handleLoginRender = (isLoggedIn) => {
+        if (isLoggedIn) {
+            return (
+                <div>
+
+
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <Link></Link>
+                    <Link></Link>
+                </div>
+            )
+        }
+    }
+
+    const handleSearchChange = (e) => {
+        e.preventDefault()
+        props.setSearchContent(e.target.value)
+    }
+
   return (
-    <div>Header</div>
+    <div>
+        <Link></Link>
+        <ul>
+            <div>
+                <div>
+                    <SearchIcon />
+                </div>
+
+                <InputBase
+                placeholder="Search..."
+                classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput
+                }}
+                inputProps={{ 'aria-label': 'search'}}
+                onChange={(e) => handleSearchChange(e)}
+                />
+            </div>
+                <ShowChartIcon className = {classes.supplementaryIcon} />
+                <Link></Link>
+                <CreateIcon className = {classes.supplementaryIcon} />
+                <Link></Link>
+                <CreateIcon className = {classes.supplementaryIcon} />
+                <Link></Link>
+                <SubjectIcon className = {classes.supplementaryIcon} />
+                <Link></Link>
+                { handleLoginRender(localStorage.getItem('auth_key') !== 'undefined')}
+        </ul>
+    </div>
+
   )
 }
 
-export default Header
+export default Header;
