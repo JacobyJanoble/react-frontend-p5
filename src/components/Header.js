81fae9d552987b859f170cca74fb71bcf382bff7
Jacob <Jacob.McCann214@gmail.com>
2022-07-13 12:38:16 -0500

    Updates to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,6 +12,108 @@ import SubjectIcon from '@material-ui/icons/Subject';
 import ShowChartIcon from '@material-ui/icons/ShowChart';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import { AccordionSummary } from '@material-ui/core';
+
+const useStyles= makeStyles((theme) => ({
+    header: {
+        display: 'flex',
+        color: 'black',
+        backgroundColor: 'white',
+    },
+    topRibbon: {
+        display: 'flex'
+    },
+    userStateButtons: {
+        marginLeft: theme.spacing(15),
+        display: 'flex'
+    },
+    ribbonItems: {
+        color: 'black',
+        fontSize: '18px',
+        textDecoration: 'none',
+        marginTop: theme.spacing(.7)
+    },
+    ribbonItemList: {
+        display: 'flex',
+        paddingInlineStart: '20px',
+      },
+      sortingRibbon: {
+        display: 'block',
+        marginLeft: theme.spacing(5)
+      },
+      inputRoot: {
+        color: 'inherit',
+        // backgroundColor: '#EFEFEF'
+      },
+      inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        // transition: theme.transitions.create('width'),
+        width: '100%',
+        // [theme.breakpoints.up('sm')]: {
+        //   width: '12ch',
+        //   '&:focus': {
+        //     width: '20ch',
+        //   },
+        // },
+      },
+      search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+          backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+          marginLeft: theme.spacing(1),
+          width: 'auto',
+        },
+        border: '1px solid',
+        borderColor: '#EDEDED'
+      },
+      searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        color: 'black'
+      },
+      readitLogoText: {
+        display: 'flex',
+        color: 'black',
+        textDecoration: 'none',
+        fontSize: '25px',
+        marginLeft: theme.spacing(3),
+        marginTop: theme.spacing(1.7)
+      },
+      readitLogo: {
+        display: 'flex',
+        color: 'white',
+        textDecoration: 'none',
+        backgroundColor: theme.palette.error.main,
+        marginRight: theme.spacing(1)
+        // fontSize: 'large',
+      },
+      supplementaryIcon: {
+        marginLeft: theme.spacing(2),
+        marginTop: theme.spacing(.7),
+        marginRight: theme.spacing(.5)
+      },
+      userStateRibbonItems: {
+        marginRight: theme.spacing(2),
+        color: 'black',
+        fontSize: '18px',
+        textDecoration: 'none',
+        marginTop: theme.spacing(.7)
+      }
+}));
+
 
 
 const Header = (props) => {
@@ -75,3 +177,5 @@ const Header = (props) => {
 }
 
 export default Header;
+
+
